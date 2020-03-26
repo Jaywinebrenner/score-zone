2ec377b4142f0a9a74f55b5529d2867d38d956cb
Jay Winebrenner <jaywinebrenner@gmail.com>
2020-03-26 08:42:58 -0700

    complete add player functionality

--- examples.js
+++ examples.js
@@ -1,5 +1,5 @@
 
-
+// component that grabs the value of an imput
 
 class ControlledInput extends Component {
   state = {
@@ -11,8 +11,8 @@ class ControlledInput extends Component {
   render() {
     return (
       <input
-        type="text" 
-        value={ this.state.value }   
+        type="text"
+        value={ this.state.value }
         onChange={ this.handleChange }
       />
     );
